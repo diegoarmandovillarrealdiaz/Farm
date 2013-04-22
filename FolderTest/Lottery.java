4d89116f353ddccbabd9dfe9e1e0b478bf78b64a
diegoarmandovillarrealdiaz <diegoarmandovillarrealdiaz@gmail.com>
2013-04-22 08:58:37 -0500

    It was added a new file.

---
+++ FolderTest/Lottery.java
@@ -0,0 +1,30 @@
+package test;
+
+public class Lottery {
+
+	public int calculateResult(int whiteBalls[], int powerBall) {
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		
+		int whiteBalls[] = new int[5];
+		int powerBall = 0;
+		
+		if (args.length == 6) {
+			for(int i=0;i<5;i++) {
+				whiteBalls[i] = Integer.parseInt(args[i]);
+			}
+			
+			powerBall = Integer.parseInt(args[5]);
+			
+			Lottery baloto = new Lottery();
+			
+			int probability = baloto.calculateResult(whiteBalls, powerBall);
+			
+			System.out.println("The probability is " + probability);			
+		}
+
+	}
+
+}
