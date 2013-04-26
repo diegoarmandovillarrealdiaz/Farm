5ae173b36119c4dee0eb82a72098848fbff9edfa
diegoarmandovillarrealdiaz <diegoarmandovillarrealdiaz@gmail.com>
2013-04-26 10:22:37 -0500

    remove some console messages.

--- Actividad_03(Final)/simple-icasa/src/main/java/co/edu/uis/sistemas/simple/icasa/SimpleIcasaComponent.java
+++ Actividad_03(Final)/simple-icasa/src/main/java/co/edu/uis/sistemas/simple/icasa/SimpleIcasaComponent.java
@@ -18,7 +18,6 @@ import org.apache.felix.ipojo.annotations.Validate;
 
 import fr.liglab.adele.icasa.device.DeviceListener;
 import fr.liglab.adele.icasa.device.GenericDevice;
-import fr.liglab.adele.icasa.device.light.BinaryLight;
 import fr.liglab.adele.icasa.device.temperature.Cooler;
 import fr.liglab.adele.icasa.device.temperature.Heater;
 import fr.liglab.adele.icasa.device.temperature.Thermometer;
@@ -27,9 +26,6 @@ import fr.liglab.adele.icasa.device.temperature.Thermometer;
 @Component(name="SimpleIcasaComponent")
 @Instantiate
 public class SimpleIcasaComponent {
-	
-	@Requires(id="lights")
-	private BinaryLight[] lights;
 	
 	@Requires(id="heaters")
 	private Heater[] heaters;
@@ -42,25 +38,21 @@ public class SimpleIcasaComponent {
 	
 	private Thread modifyLightsThread;
 	
-	@Bind(id="lights")
-	protected void bindLight(BinaryLight light) {
-		System.out.println("A new light has been added to the platform " + light.getSerialNumber());
-	}
 	
 	@Bind(id="heaters")
 	protected void bindHeater(Heater heater) {
-		System.out.println("A new heater has been added to the platform " + heater.getSerialNumber());
+		
 	}
 	
 	@Bind(id="coolers")
 	protected void bindCooler(Cooler cooler) {
-		System.out.println("A new cooler has been added to the platform " + cooler.getSerialNumber());
+		
 	}
 	
 	@Bind(id="thermometers")
 	protected void bindThermometer(Thermometer thermometer) {
 		thermometer.addListener(changeThermometerLocationLisener);
-		System.out.println("A new thermometer has been added to the platform " + thermometer.getSerialNumber());
+		
 	}
 
 	@Unbind(id="thermometers")
@@ -73,18 +65,14 @@ public class SimpleIcasaComponent {
 		List<Heater> result = new ArrayList<Heater>();
 		if(locationName != null && !locationName.equals(GenericDevice.LOCATION_UNKNOWN)){
 			List<Heater> currentHeaters = getHeaters();
-			System.out.println("Heaters number: "+currentHeaters.size());
-			System.out.println("Location: "+locationName);
+			
 			for (Heater heater : currentHeaters) {
 				String heaterLocation = (String)heater.getPropertyValue(GenericDevice.LOCATION_PROPERTY_NAME);
 				if(locationName.equals(heaterLocation)){
 					result.add(heater);
 				}
 			}
-		}{
-			System.out.println("Curioso-location:"+locationName);
 		}
-		
 		
 		return result;
 	}
@@ -93,37 +81,16 @@ public class SimpleIcasaComponent {
 		List<Cooler> result = new ArrayList<Cooler>();
 		if(locationName != null && !locationName.equals(GenericDevice.LOCATION_UNKNOWN)){
 			List<Cooler> currentCoolers = getCoolers();
-			System.out.println("Coolers number: "+currentCoolers.size());
-			System.out.println("Location: "+locationName);
 			for (Cooler cooler : currentCoolers) {
 				String heaterLocation = (String)cooler.getPropertyValue(GenericDevice.LOCATION_PROPERTY_NAME);
 				if(locationName.equals(heaterLocation)){
 					result.add(cooler);
 				}
 			}
-		}{
-			System.out.println("Curioso-location:"+locationName);
-		}
-		
-		
+		}		
 		return result;
 	}
 	
-	
-	private List<BinaryLight> getLightsIn(String locationName){
-		List<BinaryLight> result = new ArrayList<BinaryLight>();
-		if(locationName != null && !locationName.equals(GenericDevice.LOCATION_UNKNOWN)){
-			List<BinaryLight> currentHeaters = getLights();
-			for (BinaryLight heater : currentHeaters) {
-				String heaterLocation = (String)heater.getPropertyValue(GenericDevice.LOCATION_PROPERTY_NAME);
-				if(locationName.equals(heaterLocation)){
-					result.add(heater);
-				}
-			}
-		}
-		
-		return result;
-	}
 	
 	
 	private Map<String,List<Thermometer>> getAreasWithThermometer(){
@@ -144,9 +111,7 @@ public class SimpleIcasaComponent {
 		return Collections.unmodifiableList(Arrays.asList(heaters));
 	}
 	
-	protected List<BinaryLight> getLights() {
-		return Collections.unmodifiableList(Arrays.asList(lights));
-	}
+	
 	
 	protected List<Cooler> getCoolers() {
 		return Collections.unmodifiableList(Arrays.asList(coolers));
@@ -160,9 +125,6 @@ public class SimpleIcasaComponent {
 	
 	@Validate
 	public void start() {
-		modifyLightsThread = new Thread(new ModifyLigthsRunnable());
-		modifyLightsThread.start();
-		
 		searchAreasWithThermometersRunnable= new Thread(new SearchAreasWithThermometersRunnable());
 		searchAreasWithThermometersRunnable.start();
 	}
@@ -184,53 +146,24 @@ public class SimpleIcasaComponent {
 		}
 	}
 	
-	class ModifyLigthsRunnable implements Runnable {
-
-		public void run() {
-						
-			boolean running = true;
-			double step= 0.1;
-			double total = 0;
-			double max=1;
-			boolean onOff = false;
-			while (running) {
-				onOff = !onOff;
-				try {
-					List<BinaryLight> lights = getLights();
-					for (BinaryLight binaryLight : lights) {
-						binaryLight.setPowerStatus(onOff);
-					}
-					if(total<(max-step)){
-						total+=step;
-					}
-					else{
-						total=0;
-					}
-					increaseAllHeater(0.8);
-					Thread.sleep(1000);					
-				} catch (InterruptedException e) {
-					running = false;
-				}
-			}
-			
-		}
 		
-	}
-	
 	class SearchAreasWithThermometersRunnable implements Runnable {
 
 		public void run() {
 				
-			
+			System.out.println("Start Thread");
 			boolean running = true;
-
+			
 			boolean onOff = false;
 			while (running) {
+				
 				onOff = !onOff;
 				try {
 					Set<String> areas=getAreasWithThermometer().keySet();
+					
+					
 					 for(String tem:areas){
-						 double temperatur = ((Thermometer)getAreasWithThermometer().get(tem)).getTemperature();
+						 double temperatur = (getAreasWithThermometer().get(tem).get(0)).getTemperature();
 						 if(temperatur>300){
 							 List<Cooler>coolers =  getCoolerIn(tem);
 							 for(Cooler cooler: coolers){
@@ -265,7 +198,7 @@ public class SimpleIcasaComponent {
 						 getCoolerIn(tem);
 						 getHeatersIn(tem);
 					 }
-					 System.out.println("Check temperature");
+					
 					Thread.sleep(200);					
 				} catch (InterruptedException e) {
 					running = false;
@@ -289,16 +222,6 @@ public class SimpleIcasaComponent {
 		}
 		
 		System.out.println(heatersMessage);
-		
-		List<BinaryLight> lights = getLightsIn(location);
-		
-		String lightsMessage = "("+lights.size()+") Lights in ["+location+"] with the serial Numbers: ";
-		
-		for(BinaryLight light:lights){
-			lightsMessage+=light.getSerialNumber()+".";
-		}
-		
-		System.out.println(lightsMessage);
 		
 	}
 	
